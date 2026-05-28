823bd9a4e6edcb328f853a76ac0d93533c1cebc6
Nick <usr4227@salesianos-lacuesta.net>
2026-05-28 10:33:04 +0100

    quimico:"Añadir constructor"

--- src/net/salesianos/investigadores/Quimico.java
+++ src/net/salesianos/investigadores/Quimico.java
@@ -4,4 +4,9 @@ public class Quimico extends Investigador {
 
     private int nivelToxicidad;
 
+    public Quimico(String nombre, String especialidad, double sueldo, int nivelToxicidad) {
+        super(nombre, especialidad, sueldo);
+        this.nivelToxicidad = nivelToxicidad;
+    }
+
 }
