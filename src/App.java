811f8369634efa4689aad2f83621dbe948bd2aa4
Nick <usr4227@salesianos-lacuesta.net>
2026-05-28 12:41:40 +0100

    app:" Creación de dewstructura app y añdir imports de los archivos necesarios"

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
+import net.salesianos.investigadores.Matematico;
+import net.salesianos.investigadores.Biologo;
+import net.salesianos.investigadores.Quimico;
+import net.salesianos.investigadores.TrabajoDual;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+
     }
 }
