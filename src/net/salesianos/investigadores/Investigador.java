49be605186380749b7275008ad5db1a39df62af3
Nick <usr4227@salesianos-lacuesta.net>
2026-05-28 09:46:11 +0100

    Investigador:"Añadir getters y setters"

--- src/net/salesianos/investigadores/Investigador.java
+++ src/net/salesianos/investigadores/Investigador.java
@@ -1,15 +1,39 @@
-    package net.salesianos.investigadores;
+package net.salesianos.investigadores;
 
-    public abstract class Investigador {
+public abstract class Investigador {
 
-        protected String nombre;
-        protected String especialidad;
-        protected double sueldo;
+    protected String nombre;
+    protected String especialidad;
+    protected double sueldo;
 
-        public Investigador(String nombre, String especialidad, double sueldo) {
-            this.nombre = nombre;
-            this.especialidad = especialidad;
-            this.sueldo = sueldo;
-        }
-
+    public Investigador(String nombre, String especialidad, double sueldo) {
+        this.nombre = nombre;
+        this.especialidad = especialidad;
+        this.sueldo = sueldo;
     }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+
+    public double getSueldo() {
+        return sueldo;
+    }
+
+    public void setSueldo(double sueldo) {
+        this.sueldo = sueldo;
+    }
+
+}
