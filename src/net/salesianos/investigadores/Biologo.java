2234e81cbf039beee23824fc955878331792c7ba
Nick <usr4227@salesianos-lacuesta.net>
2026-05-28 10:28:18 +0100

    biologo:"añadir metodos"

--- src/net/salesianos/investigadores/Biologo.java
+++ src/net/salesianos/investigadores/Biologo.java
@@ -1,6 +1,7 @@
 package net.salesianos.investigadores;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Biologo extends Investigador {
 
@@ -19,4 +20,27 @@ public class Biologo extends Investigador {
         this.especimenes = especimenes;
     }
 
+    public void anadirEspecimen(String especimen) {
+        especimenes.add(especimen);
+    }
+
+    @Override
+    public void trabajar() {
+        if (especimenes.isEmpty()) {
+            System.out.println("No hay especímenes para procesar.");
+            return;
+        }
+
+        Collections.sort(especimenes);
+        especimenes.remove(especimenes.size() - 1);
+    }
+
+    @Override
+    public String estadoDelSueldo() {
+        if (sueldo < 1500) {
+            return "Sueldo insuficiente. Solicitar aumento.";
+        } else {
+            return "Sueldo correcto.";
+        }
+    }
 }
