9b74d35f9a87e12f4c33a8f46410d7292309c31e
Nick <usr4227@salesianos-lacuesta.net>
2026-05-28 10:17:48 +0100

    biologo:"Añadir constructor"

--- src/net/salesianos/investigadores/Biologo.java
+++ src/net/salesianos/investigadores/Biologo.java
@@ -6,4 +6,9 @@ public class Biologo extends Investigador {
 
     private ArrayList<String> especimenes;
 
+    public Biologo(String nombre, String especialidad, double sueldo, ArrayList<String> especimenes) {
+        super(nombre, especialidad, sueldo);
+        this.especimenes = especimenes;
+    }
+
 }
